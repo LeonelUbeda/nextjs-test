2e940440dfde82b234b1affc29829fedc2721216
Leonel Ubeda <leonelubeda@hotmail.com>
2020-12-18 15:15:59 -0600

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,27 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React, {useEffect, useState} from 'react'
+
+
+function Todo({title, completed}){
+  return (
+    <div className="my-2 bg-teal-200 shadow px-5 py-5 rounded-md">
+      <h1>{title}</h1>
+    </div>
+  )
+}
+
 
 export default function Home() {
-  return <h1>Hey que tal </h1>
+  const [todos, setTodos] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/')
+    .then(response => response.json())
+    .then(json => setTodos(json))
+  }, [])
+  return (
+    <>
+      {todos.map(e => <Todo title={e.title} completed={e.completed} key={e.id}/>)}
+    </>
+  )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    purge: ['./pages/**/*.js', './components/**/*.js'],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
