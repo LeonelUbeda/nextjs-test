4e99eca9172e3cfa7fa2e162e556a1db27ea7166
Leonel Ubeda <leonelubeda@hotmail.com>
2020-12-18 17:46:17 -0600

    a

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import React, {useEffect, useState} from 'react'
-
+import Link from 'next/link'
 
 function Todo({title, completed}){
   return (
@@ -12,16 +12,22 @@ function Todo({title, completed}){
 }
 
 
-export default function Home() {
-  const [todos, setTodos] = useState([])
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/')
-    .then(response => response.json())
-    .then(json => setTodos(json))
-  }, [])
+export default function Home({todos}) {
   return (
     <>
+      <h1>Hey <Link href="/contact">clic aqui</Link></h1>
       {todos.map(e => <Todo title={e.title} completed={e.completed} key={e.id}/>)}
     </>
   )
+}
+
+export async function getStaticProps(context){
+  let data = await fetch('https://jsonplaceholder.typicode.com/todos/')
+  data = await data.json()
+  return {
+    props: {
+      todos: data
+    }
+  }
+
 }
