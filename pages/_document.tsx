43b529ab354439189ee6df6b719692e23c7e1adf
Leonel Ubeda <leonelubeda@hotmail.com>
2021-02-05 22:12:08 -0600

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,35 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document'
 import Header from '../app/components/modules/Header'
-
+import { ServerStyleSheet } from 'styled-components';
 export default class CustomDocument extends Document{
+    static getInitialProps({ renderPage }) {
+        // Step 1: Create an instance of ServerStyleSheet
+        const sheet = new ServerStyleSheet();
+
+        // Step 2: Retrieve styles from components in the page
+        const page = renderPage((App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        );
+
+        // Step 3: Extract the styles as <style> tags
+        const styleTags = sheet.getStyleElement();
+
+        // Step 4: Pass styleTags as a prop
+        return { ...page, styleTags };
+    }
     render(){
-        return <Html>
-            <Head>
-                <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
-            </Head>
-            <body>
-                <Header />
-                <Main></Main>
-            </body>
-            <NextScript />
-        </Html>
+        return (
+            <Html>
+                <Head>
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+                    {this.props.styleTags}
+                </Head>
+                <body>
+                    <Header />
+                    <Main></Main>
+                </body>
+                <NextScript />
+            </Html>
+        )
     }
 }
