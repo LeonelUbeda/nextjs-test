82513803e3533db5139efbb8119cb1426e60a253
Leonel Ubeda <leonelubeda@hotmail.com>
2021-02-05 23:24:08 -0600

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,27 +2,36 @@ import Document, {Html, Head, Main, NextScript} from 'next/document'
 import Header from '../app/components/modules/Header'
 import { ServerStyleSheet } from 'styled-components';
 export default class CustomDocument extends Document{
-    static getInitialProps({ renderPage }) {
-        // Step 1: Create an instance of ServerStyleSheet
-        const sheet = new ServerStyleSheet();
-
-        // Step 2: Retrieve styles from components in the page
-        const page = renderPage((App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        );
-
-        // Step 3: Extract the styles as <style> tags
-        const styleTags = sheet.getStyleElement();
-
-        // Step 4: Pass styleTags as a prop
-        return { ...page, styleTags };
-    }
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+    
+        try {
+          ctx.renderPage = () =>
+            originalRenderPage({
+              enhanceApp: (App) => (props) =>
+                sheet.collectStyles(<App {...props} />),
+            })
+    
+          const initialProps = await Document.getInitialProps(ctx)
+          return {
+            ...initialProps,
+            styles: (
+              <>
+                {initialProps.styles}
+                {sheet.getStyleElement()}
+              </>
+            ),
+          }
+        } finally {
+          sheet.seal()
+        }
+      }
     render(){
         return (
             <Html>
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
-                    {this.props.styleTags}
                 </Head>
                 <body>
                     <Header />
